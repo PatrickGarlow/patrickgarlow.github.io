1bb824699ecd5f3a4517d4c9ec984698efc7bc80
Patrick Garlow <patrickgarlow@gmail.com>
2023-06-07 19:24:20 -0400

    V3
    Portfolio V3 Update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
-const accordionTiles = document.querySelectorAll('.accordion-title');
+function openDialog(projectId) {
+  const dialog = document.getElementById(projectId + '-dialog');
+  dialog.showModal();
+}
 
-accordionTiles.forEach((accordionTile) => {
-    accordionTile.addEventListener('click', () => {
-        const height = accordionTile.nextElementSibling.scrollHeight;
+function closeDialog(projectId) {
+  const dialog = document.getElementById(projectId + '-dialog');
+  dialog.close();
+}
 
-        accordionTile.classList.toggle('active-header');
-        if (accordionTile.classList.contains('active-header')) {
-            accordionTile.nextElementSibling.style.maxHeight = `${height}px`;
-            accordionTile.nextElementSibling.style.paddingTop = '10px';
-        }
-        else {
-            accordionTile.nextElementSibling.style.maxHeight = '0px';
-            accordionTile.nextElementSibling.style.paddingTop = '0px';
-        }
-    });
-});
+
+
+
+let list = document.querySelectorAll('.sort-list');
+let project = document.querySelectorAll('.project');
+
+for(let i = 0; i<list.length;i++) {
+  list[i].addEventListener('click', function() {
+    for(let j = 0; j < list.length; j++) {
+      list[j].classList.remove('active');
+    }
+    this.classList.add('active');  
+
+    let dataFilter = this.getAttribute('data-filter')
+    for(let k = 0; k < project.length; k++) {
+      project[k].classList.remove('active');
+      project[k].classList.add('hide');
+      if(project[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+        project[k].classList.remove('hide');
+        project[k].classList.add('active');
+      }
+    }
+  })
+}
